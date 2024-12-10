eec41d127c664bcb78452d6a7292dd4c35f22c4d
MCdragonmasters <natedragonmaster@gmail.com>
2024-12-10 16:40:52 -0500

    fr

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 ![Versions](https://img.shields.io/github/v/release/MCdragonmasters/PotatoEssentials) ![Size](https://img.shields.io/github/repo-size/MCdragonmasters/PotatoEssentials) [![Java CI with Gradle](https://github.com/MCdragonmasters/PotatoEssentials/actions/workflows/build.yml/badge.svg)](https://github.com/MCdragonmasters/PotatoEssentials/actions/workflows/build.yml)
-
+<h3>Commands</h3>
+<ul>
+<li>/gamemode &lt;gameMode&gt;<ul>
+    <li>"adventure"</li>
+    <li>"creative"</li>
+    <li>"survival"</li>
+    <li>"spectator"</li></ul></li>
+<li>/gm &lt;gameMode&gt;<ul>
+    <li>"0" (survival)</li>
+    <li>"1" (creative)</li>
+    <li>"2" (adventure)</li>
+    <li>"3" (spectator)</li>
+    <li>"a" (adventure)</li>
+    <li>"c" (creative)</li>
+    <li>"sp" (spectator)</li>
+    <li>"s" (survival)</li></ul></li>
+<li>/gma (adventure)</li>
+<li>/gmc (creative)</li>
+<li>/gmsp (spectator)</li>
+<li>/gms (survival)</li>
+</ul>
--- src/main/java/com/mcdragonmasters/potatoessentials/commands/GameModeCommand.java
+++ src/main/java/com/mcdragonmasters/potatoessentials/commands/GameModeCommand.java
@@ -3,7 +3,12 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.MultiLiteralArgument;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+
+import static com.mcdragonmasters.potatoessentials.PotatoEssentials.prefixMini;
 
 public class GameModeCommand {
     public static void register() {
@@ -19,16 +24,21 @@ public class GameModeCommand {
                     switch (args.getByArgument(gameModeArg)) {
                         case "adventure":
                             p.setGameMode(GameMode.ADVENTURE);
+                            break;
                         case "creative":
                             p.setGameMode(GameMode.CREATIVE);
+                            break;
                         case "spectator":
                             p.setGameMode(GameMode.SPECTATOR);
+                            break;
                         case "survival":
                             p.setGameMode(GameMode.SURVIVAL);
+                            break;
                         case null, default:
+                            Bukkit.broadcast(Component.text("Err: idk"));
                             break;
                     }
-                    p.sendRichMessage("<gray>Set Your Gamemode to<yellow> " + p.getGameMode().name());
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
                 })
                 .register();
         new CommandAPICommand("gm")
@@ -38,17 +48,53 @@ public class GameModeCommand {
                     switch (args.getByArgument(gmArg)) {
                         case "a", "2":
                             p.setGameMode(GameMode.ADVENTURE);
+                            break;
                         case "c", "1":
                             p.setGameMode(GameMode.CREATIVE);
+                            break;
                         case "sp", "3":
                             p.setGameMode(GameMode.SPECTATOR);
+                            break;
                         case "s", "0":
                             p.setGameMode(GameMode.SURVIVAL);
+                            break;
                         case null, default:
+                            Bukkit.broadcast(Component.text("Err: idk"));
                             break;
                     }
-                    p.sendRichMessage("<gray>Set Your Gamemode to<yellow> " + p.getGameMode().name());
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
                 })
                 .register();
+        new CommandAPICommand("gma")
+                .withPermission(permission)
+                .executesPlayer((p, args) -> {
+                    p.setGameMode(GameMode.ADVENTURE);
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
+                })
+                .register();
+        new CommandAPICommand("gmc")
+                .withPermission(permission)
+                .executesPlayer((p, args) -> {
+                    p.setGameMode(GameMode.CREATIVE);
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
+                })
+                .register();
+        new CommandAPICommand("gmsp")
+                .withPermission(permission)
+                .executesPlayer((p, args) -> {
+                    p.setGameMode(GameMode.SPECTATOR);
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
+                })
+                .register();
+        new CommandAPICommand("gms")
+                .withPermission(permission)
+                .executesPlayer((p, args) -> {
+                    p.setGameMode(GameMode.SURVIVAL);
+                    sendGameModeMessage(p, p.getGameMode().translationKey());
+                })
+                .register();
+    }
+    private static void sendGameModeMessage(Player p, String translationKey) {
+        p.sendRichMessage(prefixMini +"<gray> Set Your Gamemode to<yellow> <translate:"+translationKey+">");
     }
 }
